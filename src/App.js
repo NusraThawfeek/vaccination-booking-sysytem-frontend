49e82514a02e2e29e0bdcc4f23a7f37c3a16b747
Nusra98 <Nusra98@users.noreply.github.com>
2021-09-24 01:24:28 +0530

    admin interface

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom'
+import AdminGs from './components/admin/AdminGs';
+import AdminMoh from './components/admin/AdminMoh';
+import Hero from './components/Hero';
+import AdminHome from './components/admin/AdminHome';
 
 function App() {
   return (
     <div className="App">
    
-     
+<Router>
+<Navbar />
+ <Switch>
+ <Route exact path='/admin' component={AdminHome}/>
+   <Route exact path='/assign-gs' component={AdminGs}/>
+   <Route exact path='/assign-moh' component={AdminMoh}/>
+ </Switch>
+</Router>
     </div>
   );
 }
---
+++ src/components/admin/AdminHome.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Hero from '../Hero'
+
+function AdminHome() {
+    return (
+        <div className=''>
+            <Hero />
+            <div className="card card-home margin" >
+                <div className='card-content '>
+                    <ul className="list-admi d-flex justify-content-center">
+                        <li className='listitem-admi'><Link to='/assign-gs'>Assign GS</Link></li>
+                        <li className='listitem-admi'><Link to='/assign-moh'>Assign MOH</Link></li>
+                    </ul>
+
+                </div>
+               
+            </div>
+            <div className='card card-home'>
+            <div className='card-content p-5'>
+            <h4 className='pb-3' style={{textAlign:'center'}}>Upcomming Vaccination Details</h4>
+            <table class=" table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">date</th>
+                            <th scope="col">Center</th>
+                            <th scope="col">GS division</th>
+                            <th scope="col">MOH division</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
+                </div>
+        </div>
+    )
+}
+
+export default AdminHome
