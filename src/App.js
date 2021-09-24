fa7df08dfa31cf2daf0c439e82ba25d1b9e62925
Nifla1998 <nifla.18@itfac.mrt.ac.lk>
2021-09-24 20:06:51 +0530

    created component to GS

--- src/App.js
+++ src/App.js
@@ -4,33 +4,38 @@ import "./App.css";
 import LoginForm from "./components/LoginForm/LoginForm";
 import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import GSHome from "./components/GSHome/GSHome";
 
 
+  function
 
-function App() {
-  const [title, updateTitle] = useState(null);
+  App() {
+    const [title, updateTitle] = useState(null);
 
-  const [errorMessage, updateErrorMessage] = useState(null);
+    const [errorMessage, updateErrorMessage] = useState(null);
 
-  return (
-      <Router>
-        <div className="App">
-          <div className="container d-flex align-items-center flex-column">
-            <Switch>
-              <Route path="/" exact={true}>
-                <LoginForm
-                    showError={updateErrorMessage}
-                    updateTitle={updateTitle}
-                />
-              </Route>
-              <Route path="/home">
-                <Home />
-              </Route>
-            </Switch>
+    return (
+        <Router>
+          <div className="App">
+            <div className="container d-flex align-items-center flex-column">
+              <Switch>
+                <Route path="/" exact={true}>
+                  <LoginForm
+                      showError={updateErrorMessage}
+                      updateTitle={updateTitle}
+                  />
+                </Route>
+                <Route path="/home">
+                  <Home/>
+                </Route>
+                <Route path="/gshome">
+                  <GSHome/>
+                </Route>
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
-  );
-}
+        </Router>
+    );
+  }
 
 export default App;
